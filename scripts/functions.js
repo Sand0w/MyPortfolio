c5cd558d24b36cb8f7f49b8d3a86a39a2682b4c9
Thomas Bronniart <thomas.bronniart@gmail.com>
2020-03-30 17:02:37 +0200

    First push, Hello there

---
+++ scripts/functions.js
@@ -0,0 +1,76 @@
+function animate()
+{
+    document.getElementsByClassName('overlay').setAttribute("class", "animation");
+}
+
+$(document).on('click', '.open-options', function(event) {
+  event.preventDefault();
+  $('#modal-options').iziModal('open');
+});
+
+$('#modal-options').iziModal({
+  headerColor: '#0097CA',
+  overlayColor: 'rgba(0, 0, 0, 0.5)',
+  fullscreen: true,
+  transitionIn: 'fadeInUp',
+  transitionOut: 'fadeOutDown',
+  iframe: true,
+  iframeHeight: 600,
+  width: 600,
+  iframeURL: 'https://www.netfirst.fr'
+});
+
+
+$(document).on('click', '.open-options-2', function(event) {
+  event.preventDefault();
+  $('#modal-options-2').iziModal('open');
+});
+
+$('#modal-options-2').iziModal({
+  headerColor: '#0097CA',
+  overlayColor: 'rgba(0, 0, 0, 0.5)',
+  fullscreen: true,
+  transitionIn: 'fadeInUp',
+  transitionOut: 'fadeOutDown',
+  iframe: true,
+  iframeHeight: 600,
+  width: 600,
+  iframeURL: 'http://www.taker.epitech.eu'
+});
+
+$(document).on('click', '.open-options-3', function(event) {
+  event.preventDefault();
+  $('#modal-options-3').iziModal('open');
+});
+
+$('#modal-options-3').iziModal({
+  headerColor: '#0097CA',
+  overlayColor: 'rgba(0, 0, 0, 0.5)',
+  fullscreen: true,
+  transitionIn: 'fadeInUp',
+  transitionOut: 'fadeOutDown',
+  iframe: true,
+  iframeHeight: 600,
+  width: 600,
+  iframeURL: 'https://www.easyvoyage.com'
+});
+
+/*$(document).on('click', '.open-options-4', function(event) {
+  event.preventDefault();
+  $('#modal-options-2').iziModal('open');
+});
+
+$('#modal-options-4').iziModal({
+  headerColor: '#0097CA',
+  overlayColor: 'rgba(0, 0, 0, 0.5)',
+  fullscreen: true,
+  transitionIn: 'fadeInUp',
+  transitionOut: 'fadeOutDown',
+  iframe: true,
+  iframeHeight: 600,
+  width: 600,
+  iframeURL: 'http://taker.epitech.eu/'
+});*/
+
+
+
